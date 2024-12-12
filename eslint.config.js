b3d1b1cadd4544ca518f8b89a35a3066e90d7aca
Tim Betschart <tim.betschart@icloud.com>
2024-12-12 09:26:04 +0100

    linter test

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,16 @@ import pluginJs from '@eslint/js'
 /** @type {import('eslint').Linter.Config[]} */
 export default [
     { languageOptions: { globals: globals.browser } },
-    pluginJs.configs.recommended,
+    pluginJs.configs.recommended, {
+        rules: {
+            // Aktiviert die Regel für ungenutzte Variablen
+            'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+
+            // Erzwingt die Verwendung von const, wenn Variablen nicht neu zugewiesen werden
+            'prefer-const': 'error',
+
+            // Erzwingt die Verwendung von `===` und `!==` anstelle von `==` und `!=`
+            'eqeqeq': ['error', 'always'],
+        },
+    }
 ]
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import chalk from 'chalk'
 
 console.log(chalk.blue('It works'))
+
+let myUnusedVariable = ''
+
+let check = true
+if (check == false) {
+    check = true
+}
